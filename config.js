2ea77f84f79eaf19196c9322b29701928d2bac4a
dineshr0610 <bobby06102005@gmail.com>
2026-02-17 22:21:21 +0530

    Update API URL for production

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-    ? 'http://localhost:5000'
-    : 'https://vibesync-api.onrender.com';
+const API_URL = 'https://vibesync-backend-4vap.onrender.com' //window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'? 'http://localhost:5000': 'https://vibesync-api.onrender.com';
